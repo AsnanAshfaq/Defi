13cd6167da78d205543f6ed37ab5ff4184093eb7
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2022-01-24 21:18:04 +0500

    Fixed some bugs

--- src/Screens/Main/Posts.tsx
+++ src/Screens/Main/Posts.tsx
@@ -17,12 +17,6 @@ import PostCard from '../../Components/PostCard';
 import firestore from '@react-native-firebase/firestore';
 import Loading from '../../Components/Loading';
 
-const data = [
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also",
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also",
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also",
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also",
-];
 const Posts: FC = () => {
   const [posts, setPosts] = useState<Array<{id: string; text: string}>>([]);
   const [refreshing, setrefreshing] = useState(false);
@@ -46,56 +40,23 @@ const Posts: FC = () => {
     }
   };
 
-  const getPosts = async () => {
-    setloading(true);
-    firestore()
-      .collection('posts')
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(documentSnapshot => {
-          console.log('Doc is', documentSnapshot.id);
-          setPosts(prev => {
-            return [
-              ...prev,
-              {
-                id: documentSnapshot.id,
-                text: documentSnapshot.data().text,
-              },
-            ];
-          });
-        });
-      })
-      .then(() => setloading(false))
-      .catch(() => setloading(false));
-  };
-
-  const onRefresh = () => {
-    setrefreshing(true);
-    getPosts();
-    setrefreshing(false);
-  };
-
   useEffect(() => {
     const subscribe = firestore()
       .collection('posts')
       .onSnapshot(documentSnapshot => {
         documentSnapshot.docs.map(doc => {
-          setPosts(prev => {
-            return [
-              {
-                id: doc.id,
-                text: doc.data().text,
-              },
-              ...prev,
-            ];
-          });
+          setPosts(
+            documentSnapshot.docs.map(item => ({
+              id: item.id,
+              text: item.data().text,
+            })),
+          );
         });
-
-        // console.log('User data: ', documentSnapshot.docs);
       });
 
     return () => subscribe();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [firestore]);
   return (
     <View
       style={[
@@ -122,13 +83,6 @@ const Posts: FC = () => {
       ) : (
         <FlatList
           data={posts}
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              onRefresh={onRefresh}
-              progressBackgroundColor={Colors.WHITE}
-            />
-          }
           style={{marginTop: Height * 0.03}}
           renderItem={({item}) => <PostCard text={item.text} />}
           keyExtractor={(item, index) => `${item.id}-${index}`}
